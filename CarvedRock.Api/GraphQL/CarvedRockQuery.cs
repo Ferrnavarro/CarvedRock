050f0350d1ff1d014c17fb980a756e57ad212a96
ferrnavarro <fer.maganavarro@gmail.com>
2020-04-09 22:10:54 -0600

    Writing a Schema

---
+++ CarvedRock.Api/GraphQL/CarvedRockQuery.cs
@@ -0,0 +1,21 @@
+﻿using CarvedRock.Api.Data;
+using CarvedRock.Api.Interfaces;
+using GraphQL.Types;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CarvedRock.Api.GraphQL
+{
+    public class CarvedRockQuery: ObjectGraphType
+    {
+        public CarvedRockQuery(IProductRepository productRepository)
+        {
+            Field<ListGraphType<Types.ProductType>>(
+                "products",
+                resolve: context => productRepository.GetAll()
+                );
+        }
+    }
+}
