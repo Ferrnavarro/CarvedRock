9685d22543784eebfe15cb288a845cf0d13d32a7
ferrnavarro <fer.maganavarro@gmail.com>
2020-04-09 21:51:04 -0600

    Add project files.

---
+++ CarvedRock.Api/Interfaces/IProductRepository.cs
@@ -0,0 +1,13 @@
+﻿using CarvedRock.Api.Data.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CarvedRock.Api.Interfaces
+{
+    public interface IProductRepository
+    {
+        IEnumerable<Product> GetAll();
+    }
+}
---
+++ CarvedRock.Api/Repositories/ProductRepository.cs
@@ -0,0 +1,25 @@
+﻿using CarvedRock.Api.Data;
+using CarvedRock.Api.Data.Entities;
+using CarvedRock.Api.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CarvedRock.Api.Repositories
+{
+    public class ProductRepository: IProductRepository
+    {
+        private readonly CarvedRockDbContext _dbContext;
+
+        public ProductRepository(CarvedRockDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public IEnumerable<Product> GetAll()
+        {
+            return _dbContext.Products;
+        }
+    }
+}
